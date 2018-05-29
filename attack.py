54e2a0a224b69355f6e383073988ad571ce0e7e9
shubham jain <shubhamjain2908@gmail.com>
2018-05-30 01:37:50 +0530

    creating Class and its object

--- attack.py
+++ attack.py
@@ -1,6 +1,22 @@
 import random
 
 
+class Enemy:
+    atkl = 60
+    atkh = 80
+
+    # 'self' is the entire object of this class
+    # 'self' is analogous to 'this'
+    def getAtk(self):
+        print(self.atkl)
+
+enemy1 = Enemy()    # creating object of Enemy classs
+enemy1.getAtk()
+
+enemy2 = Enemy()
+enemy2.getAtk()
+'''
+
 playerhp = 260
 enemyatkl = 60
 enemyatkh = 80
@@ -19,3 +35,5 @@ while playerhp > 0:
 
     print("You have low health: You have been teleported to somewhere else")
     break
+
+'''
