0c6a6e62abde309cde3961967a7a968fdb770143
shubham jain <shubhamjain2908@gmail.com>
2018-05-30 15:48:09 +0530

    added characters & utility classes

--- classes/game.py
+++ classes/game.py
@@ -17,10 +17,56 @@ class Person:
         self.maxmp = mp
         self.mp = mp
         self.atkl = atk - 10
-        self.atlh = atk + 10
+        self.atkh = atk + 10
         self.df = df
         self.magic = magic
         self.actions = ["Attack", "Magic"]
 
     def generate_damage(self):
         return random.randrange(self.atkl, self.atkh)
+
+    def generate_spell_damage(self, i):
+        ngl = self.magic[i]["dmg"] - 5
+        ngh = self.magic[i]["dmg"] + 5
+        return random.randrange(ngl, ngh)
+
+    def take_damage(self, dmg):
+        self.hp -= dmg
+        if self.hp < 0:
+            self.hp = 0
+        return self.hp
+
+    def get_hp(self):
+        return self.hp
+
+    def get_max_hp(self):
+        return self.maxhp
+
+    def get_mp(self):
+        return self.mp
+
+    def get_max_mp(self):
+        return self.maxmp
+
+    def reduce_mp(self, cost):
+        self.mp -= cost
+
+    def get_spell_name(self, i):
+        return self.magic[i]["name"]
+
+    def get_spell_mp_cost(self, i):
+        return self.magic[i]["cost"]
+
+    def choose_action(self):
+        i =1
+        print("Actions")
+        for item in self.actions:
+            print(str(i) + ":", item)
+            i += 1
+
+    def choose_magic(self):
+        i =1
+        print("Magic")
+        for spell in self.magic:
+            print(str(i) + ":", spell["name"], "(cost:", str(spell["np"]) + ")")
+            i += 1
