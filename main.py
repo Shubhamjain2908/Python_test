1ac305d0d007ae7bbd42ac981066e6649105196e
shubham jain <shubhamjain2908@gmail.com>
2018-06-02 00:49:38 +0530

    Better HP view

--- main.py
+++ main.py
@@ -2,7 +2,21 @@ from classes.game import Person, bcolors
 from classes.magic import Spell
 from classes.inventory import Item
 
+print("\n\n")
+print("NAME                     HP                                  MP")
+print("                          ________________________           _________ ")
+print(bcolors.BOLD + "Valos :          " +
+      " 10/460 |" + bcolors.OKGREEN + "██████               " + bcolors.ENDC + bcolors.BOLD
+      + "|   " +
+      "65/65|" + bcolors.OKBLUE + "██████" + bcolors.ENDC + "|")
 
+print("                          ________________________           _________ ")
+print("Valos :          460/460 |                        |    65/65|         |")
+
+print("                          ________________________           _________ ")
+print("Valos :          460/460 |                        |    65/65|         |")
+
+print("\n\n")
 # Create Black Magic
 fire = Spell("Fire", 10, 100, "black")
 thunder = Spell("Thunder", 10, 100, "black")
