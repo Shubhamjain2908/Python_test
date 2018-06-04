9d07f1b1050cec1412a49f6408ed2aaeb5c0e080
shubham jain <shubhamjain2908@gmail.com>
2018-06-05 02:34:21 +0530

    Posting JSON data with headers

--- requestPostJSON.py
+++ requestPostJSON.py
@@ -3,7 +3,9 @@ import simplejson as json
 
 url = "https://www.googleapis.com/urlshortener/v1/url"
 payload = {"longUrl": "http://www.google.com/"}
-headers = {"Content-Type": "application/json"}
+headers = {
+    "Content-Type": "application/json"
+    }
 r = requests.post(url, json=payload, headers=headers)
 
 print(json.loads(r.text)["error"]["code"])
