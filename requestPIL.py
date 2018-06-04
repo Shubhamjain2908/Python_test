02d342b81f3e660eab4a902a6a6146b58eea8b77
shubham jain <shubhamjain2908@gmail.com>
2018-06-05 02:38:38 +0530

    web scrapping

--- requestPIL.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-my_data = { "firstname":"Mickey" ,"lastname":"Mouse" }
-r = requests.post("https://www.w3schools.com/html/tryit.asp?filename=tryhtml_form_submit/action_page.php", my_data)
-
-f = open("myFile.html", "w+")
-f.write(r.text)
--- writeFile.py
+++
@@ -1,5 +0,0 @@
-newFile = open("newFile.txt", "w+") # 2 parameters file to create # file behaviour read or write
-
-string = "This is the new file content"
-
-newFile.write(string)
