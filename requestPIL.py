c7d89410acdd0fec4c1187f3ebf74ab658ad2d7b
shubham jain <shubhamjain2908@gmail.com>
2018-06-05 01:49:20 +0530

    Using Pillow the image processing library (PIL)

---
+++ requestPIL.py
@@ -0,0 +1,18 @@
+import requests
+from io import BytesIO
+from PIL import Image
+
+r = requests.get("https://wallpapercave.com/wp/grWD6zh.jpg")
+
+print("Status Code: ", r.status_code)
+
+image = Image.open(BytesIO(r.content))
+
+print(image.size, image.format, image.mode)
+path = "./image."  + image.format
+
+
+try:
+    image.save(path, image.format)
+except IOError:
+    print("Cannot save image")
